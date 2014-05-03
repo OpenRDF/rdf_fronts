8b7728c957c7a48ffbdb613258fa768556b25516
Xianbin Xie <a.b@hotmail.com>
2014-05-03 23:26:54 +0800

    update baseaction.java

---
+++ rdf_fronts/business/com/openrdf/base/action/BaseAction.java
@@ -0,0 +1,40 @@
+package com.openrdf.base.action;
+
+import com.openrdf.utils.url.properties.URLPropertiesUtils;
+
+public class BaseAction extends OpenRDFBaseAction {
+
+	/**
+	 * BaseAction 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String rdf_fronts_href = "";
+	private String rdf_cas_href = "";
+
+	public String toIndexPage(){
+		rdf_cas_href = URLPropertiesUtils.getPropertiesUrl(URLPropertiesUtils.RDF_CAS_HREF);
+		return "success";
+	}
+	
+	public String toLoginPage(){
+		// redirect to register page
+		rdf_cas_href = URLPropertiesUtils.getPropertiesUrl(URLPropertiesUtils.RDF_CAS_HREF);
+		return "success";
+	}
+	
+	public String toRegisterPage(){
+		// redirect to login page 
+		rdf_fronts_href = URLPropertiesUtils.getPropertiesUrl(URLPropertiesUtils.RDF_FRONTS_HREF);
+		rdf_cas_href = URLPropertiesUtils.getPropertiesUrl(URLPropertiesUtils.RDF_CAS_HREF);
+		return "success";
+	}
+
+	// ==================== getters ===========================//
+	public String getRdf_fronts_href() {
+		return rdf_fronts_href;
+	}
+
+	public String getRdf_cas_href() {
+		return rdf_cas_href;
+	}
+}
