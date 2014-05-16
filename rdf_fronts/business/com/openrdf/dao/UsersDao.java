4893959bb977e23fcbc563827a77517eb862968c
Xianbin Xie <a.b@hotmail.com>
2014-05-16 20:43:14 +0800

    add modify user method

--- rdf_fronts/business/com/openrdf/dao/UsersDao.java
+++ rdf_fronts/business/com/openrdf/dao/UsersDao.java
@@ -1,14 +1,51 @@
 package com.openrdf.dao;
 
+import java.util.List;
+
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
+import com.openrdf.beans.User;
 import com.openrdf.beans.UserLogin;
 
 public class UsersDao extends HibernateDaoSupport {
 
-	// add
-	public void addUser(UserLogin useLogin) {
+	// add addUserLogin
+	public void addUserLogin(UserLogin useLogin) {
 		this.getHibernateTemplate().save(useLogin);
+	}
+
+	// add User
+	public void addUser(User user) {
+		this.getHibernateTemplate().save(user);
+	}
+
+	// get user by id
+	public User getUserById(String userId) {
+		User user = null;
+		user = this.getHibernateTemplate().get(User.class, userId);
+		return user;
+	}
+
+	// get user by LoginEmail
+	@SuppressWarnings("unchecked")
+	public UserLogin getUserLoginByUserLoginEmail(String userLoginEmail) {
+		String sql = "From UserLogin where userLoginEmail = '" + userLoginEmail
+				+ "'";
+		List<Object> userLoginList = this.getHibernateTemplate().find(sql);
+		if(userLoginList.size() == 0){
+			return null;
+		}
+		return (UserLogin) userLoginList.get(0);
+	}
+
+	// update user
+	public void updateUser(User user) {
+		this.getHibernateTemplate().update(user);
+	}
+
+	// delete user
+	public void deleteUser(User user) {
+		this.getHibernateTemplate().delete(user);
 	}
 
 }
--- rdf_fronts/business/com/openrdf/service/UsersService.java
+++ rdf_fronts/business/com/openrdf/service/UsersService.java
@@ -2,31 +2,82 @@ package com.openrdf.service;
 
 import org.apache.log4j.Logger;
 
-import com.openrdf.beans.UserLogin;
 import com.openrdf.beans.User;
+import com.openrdf.beans.UserLogin;
 import com.openrdf.dao.UsersDao;
+import com.openrdf.utils.Utils;
 
 public class UsersService {
-	
+
 	private Logger logger = Logger.getLogger(UsersService.class);
 	private UsersDao usersDao;
 
-	// add 
+	/**
+	 *  add
+	 *  
+	 * @param userLogin
+	 */
 	public void addUser(UserLogin userLogin) {
-		
-		usersDao.addUser(userLogin);
-		
+		// 向UserLogin中添加数据
+		usersDao.addUserLogin(userLogin);
+		// 向Users中添加数据
+		String userName = userLogin.getUserLoginEmail().substring(0,
+				userLogin.getUserLoginEmail().indexOf("@"));
+		User user = new User(userLogin.getUserId(), userName, "", "", "",
+				Utils.getCurrentTimeMillis(), 114, "1900-01-01", "", "", "",
+				"", "", "", userLogin.getUserLoginEmail(), 0, "", "", "", "",
+				"", "", 0, "");
+		usersDao.addUser(user);
+	}
+
+	// delete
+
+	// update
+
+	// select
+
+
+	/**
+	 *  认证邮箱
+	 *  
+	 * @param userLoginEmail
+	 * @param userId
+	 * @return
+	 */
+	public String emailAuthentication(String userLoginEmail, String userId) {
+		User user = usersDao.getUserById(userId);
+		if(user == null){
+			return "userNotFound";
+		}else if(user.getUserEmail().equalsIgnoreCase(userLoginEmail)){
+			if(user.getEmailAuthentication() == 0){
+				user.setEmailAuthentication(1);
+				usersDao.updateUser(user);
+				return "authSuccess";
+			}else{
+				return "reEmailAuth";
+			}
+		}
+		return null;
 	}
 	
-	// delete 
+	/**
+	 * 判断邮箱是否存在 
+	 * 
+	 * @param userLoginEmail
+	 * @return boolean
+	 */
+	public boolean isExistUserLogin(String userLoginEmail){
+		UserLogin userLogin = usersDao.getUserLoginByUserLoginEmail(userLoginEmail);
+		if(userLogin == null){
+			// 登录邮箱不存在
+			return false;
+		}else{
+			// 登录邮箱存在 
+			return true;
+		}
+	}
 	
-	
-	// update 
-	
-	
-	// select 
-	
-	
+	// ========================= getters and setters =========================== //
 	public UsersDao getUsersDao() {
 		return usersDao;
 	}
